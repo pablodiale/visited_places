5d0c27eb88778f30ac3e94b8d7cf6f1e7d621ea5
Pablo Diale <pdiale@ascentio.com.ar>
2024-09-03 11:39:30 -0300

    Updated according to the countries I have visited.

--- countries.js
+++ countries.js
@@ -1,7 +1,23 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
+    ARG: {fillKey: 'visited'},
+    BOL: {fillKey: 'visited'},
+    PER: {fillKey: 'visited'},
+    CHL: {fillKey: 'visited'},
+    CAN: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
     ITA: {fillKey: 'visited'},
+    //VAT: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    BRA: {fillKey: 'visited'},
+    MEX: {fillKey: 'visited'},
     USA: {fillKey: 'visited'},
-    THA: {fillKey: 'visited'},
+    URY: {fillKey: 'visited'},
+    JPN: {fillKey: 'visited'},
+    KOR: {fillKey: 'visited'},
+    PHL: {fillKey: 'visited'},
+    //HKG: {fillKey: 'visited'},
+    //MAC: {fillKey: 'visited'},
+    CHN: {fillKey: 'visited'},
 }
